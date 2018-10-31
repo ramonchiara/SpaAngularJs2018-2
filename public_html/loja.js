7be46d801be4e7cecc52e78920861601cfa2d8b3
Ramon Chiara <ramonchiara@gmail.com>
2018-10-31 00:53:33 -0200

    Código da aula do dia 24/10/2018.

--- public_html/loja.js
+++ public_html/loja.js
@@ -1,17 +1,74 @@
-var app = angular.module('LojaApp', []);
+var app = angular.module('LojaApp', ['ui.router']);
 
-app.controller('PrincipalCtrl', function ($scope, $http) {
+app.config(function ($stateProvider, $urlRouterProvider) {
+
+    $stateProvider.state('principal', {
+        url: '/',
+        templateUrl: 'views/principal.html'
+    });
+
+    $stateProvider.state('produto', {
+        url: '/produtos/:id',
+        templateUrl: 'views/produto.html',
+        controller: 'ProdutoCtrl'
+    });
+
+    $urlRouterProvider.otherwise('/');
+
+});
+
+app.controller('PrincipalCtrl', function ($scope, $produtoDao) {
 
     $scope.min = 0;
     $scope.max = 10000;
     $scope.produtos = [];
 
-    $http.get('produtos.json').then(function (response) {
-        $scope.produtos = response.data;
+    $produtoDao.getProdutos().then(function (produtos) {
+        $scope.produtos = produtos;
     });
+
+    // $http.get('produtos.json').then(function (response) {
+    //     $scope.produtos = response.data;
+    // });
 
     $scope.minmax = function (produto) {
         return produto.preco >= $scope.min && produto.preco <= $scope.max;
+    };
+
+});
+
+app.controller('ProdutoCtrl', function ($scope, $produtoDao, $stateParams) {
+
+    $scope.produto = $produtoDao.getProdutoById($stateParams.id);
+
+});
+
+app.factory('$produtoDao', function ($http, $q) {
+
+    var produtos = [];
+
+    function getProdutos() {
+        return $q(function (resolve, reject) {
+            $http.get('produtos.json').then(function (response) {
+                produtos = response.data;
+                resolve(produtos);
+            });
+        });
     }
+
+    function getProdutoById(id) {
+        for (var i = 0; i < produtos.length; i++) {
+            var produto = produtos[i];
+            if (produto.id == id) {
+                return produto;
+            }
+        }
+        return null;
+    }
+
+    return {
+        getProdutos,
+        getProdutoById
+    };
 
 });
